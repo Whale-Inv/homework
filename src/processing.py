6c19b05d66462a6d824cb529b25ca9f87d102056
Nikita Dorozhko <nick.dorozhko@yandex.ru>
2025-08-17 18:47:14 +0400

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,9 @@ def filter_by_state(user_date: list, state: str = "EXECUTED") -> list:
         if elem.get("state") == state:
             new_user_date.append(elem)
     return new_user_date
+
+
+def sort_by_date(user_date: list, reverse_sort: bool = True) -> list:
+    # Используем get_date напрямую в sorted()
+    sorted_data = sorted(user_date, key=lambda x: x["date"], reverse=reverse_sort)
+    return sorted_data
