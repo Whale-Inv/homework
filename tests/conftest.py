94e8fe0cc5b9fcdb4acb54bca029d929c453b4b3
Nikita Dorozhko <nick.dorozhko@yandex.ru>
2025-08-24 13:38:51 +0400

    tests directory and modules

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+
+
+@pytest.fixture
+def incorrect_card_number() -> str:
+    return "некорректный номер карты"
+
+
+@pytest.fixture
+def incorrect_account_number() -> str:
+    return "некорректный номер счета"
+
+
+@pytest.fixture
+def incorrect_account_card() -> str:
+    return "введены некорректные данные"
+
+
+@pytest.fixture
+def dict_transaction_data() -> list[dict]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def operation_list_default() -> list[dict]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def reverse_false_list() -> list[dict]:
+    return [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
---
+++ tests/test_widget.py
@@ -0,0 +1,60 @@
+from typing import Callable
+
+import pytest
+from _pytest.raises import raises
+
+from src.widget import get_date, mask_account_card
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Maestro 7000792289606361", "Maestro 7000 79** **** 6361"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
+def test_mask_account_card(value: str, expected: str) -> None:
+    assert mask_account_card(value) == expected
+
+
+def test_mask_account_card_incorrect(incorrect_account_card: Callable[[], str]) -> None:
+    assert mask_account_card(11) == incorrect_account_card
+
+    assert mask_account_card(" ") == incorrect_account_card
+
+    assert mask_account_card("aasdfqwer") == incorrect_account_card
+
+
+@pytest.mark.parametrize(
+    "date, format_date",
+    [
+        ("2025-08-24T12:34:56.123", "24.08.2025"),
+        ("2025-01-01T00:00:00.000", "01.01.2025"),
+        ("2025-12-31T23:59:59.999", "31.12.2025"),
+    ],
+)
+def test_get_date(date: str, format_date: str) -> None:
+    assert get_date(date) == format_date
+
+    assert get_date(date) == format_date
+
+    assert get_date(date) == format_date
+
+
+def test_get_date_error() -> None:
+
+    with raises(ValueError):
+        get_date("24.08.2025")
+
+    with raises(ValueError):
+        get_date("2025/08/24T12:34:56.123")
+
+    with raises(ValueError):
+        get_date("2025-02-30T12:34:56.123")
+
+    with raises(ValueError):
+        get_date("2025-13-01T12:34:56.123")
+
+    with raises(ValueError):
+        get_date("")
