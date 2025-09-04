23d3be643f7c8d62b2b7612bcb340d47f954fb68
Nikita Dorozhko <nick.dorozhko@yandex.ru>
2025-09-04 20:50:35 +0400

    add docstring to generators.py and add parametrize to test generators function

--- src/generators.py
+++ src/generators.py
@@ -2,16 +2,29 @@ from typing import Generator
 
 
 def filter_by_currency(transactions: list, currency: str = "USD") -> list:
+    """
+    Функция принимает на входе список транзакций и валюту (по умолчанию 'USD', опционально)
+    и возвращает список отфильтрованный по заданной валюте
+    """
     return list(filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions))
 
 
 def transaction_descriptions(transactions: list) -> Generator:
+    """
+    Функция принимает на входе список транзакций и
+    возвращает итерируемый объект, который может генерировать значения по запросу,
+    выдавая описание транзакций
+    """
     for transaction in transactions:
         yield transaction["description"]
 
 
 def card_number_generator(start: int, stop: int) -> Generator:
-
+    """
+    Функция принимает на входе два целочисленных значения - начала и конца генерации.
+    Генерирует номера карт от заданного начального значения до конечного по формату:
+    XXXX XXXX XXXX XXXX
+    """
     if not isinstance(start, int) or not isinstance(stop, int):
         raise TypeError("Номер карты может содержать только числовые значения")
 
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -57,26 +57,10 @@ def test_card_number_generator(card_gen_result: list) -> None:
     gen = card_number_generator(9999999999999999, 9999999999999999)
     assert next(gen) == "9999 9999 9999 9999"
 
-
-def test_card_number_generator_type_errors() -> None:
+@pytest.mark.parametrize('start, stop', [(1, '3'), ('1', 3), ('1', '3'), (1.5, 3), (1, None)])
+def test_card_number_generator_type_errors(start, stop) -> None:
     with pytest.raises(TypeError) as excinfo:
-        list(card_number_generator(1, "3"))
-    assert str(excinfo.value) == "Номер карты может содержать только числовые значения"
-
-    with pytest.raises(TypeError) as excinfo:
-        list(card_number_generator("1", 3))
-    assert str(excinfo.value) == "Номер карты может содержать только числовые значения"
-
-    with pytest.raises(TypeError) as excinfo:
-        list(card_number_generator("1", "3"))
-    assert str(excinfo.value) == "Номер карты может содержать только числовые значения"
-
-    with pytest.raises(TypeError) as excinfo:
-        list(card_number_generator(1.5, 3))
-    assert str(excinfo.value) == "Номер карты может содержать только числовые значения"
-
-    with pytest.raises(TypeError) as excinfo:
-        list(card_number_generator(1, None))
+        list(card_number_generator(start, stop))
     assert str(excinfo.value) == "Номер карты может содержать только числовые значения"
 
 
