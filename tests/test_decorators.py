f277c46a50b2bd4743359d10ebad29c73d57b6ca
Nikita Dorozhko <nick.dorozhko@yandex.ru>
2025-09-11 19:01:07 +0400

    modified module test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -42,3 +42,24 @@ def test_writing_file():
         assert content == "add_numbers ok", f"Неверное содержимое файла: {content}"
 
     os.remove(test_file_path)
+
+
+def test_writing_file_wrong():
+    test_file_path = "tests/qwer.txt"
+
+    @log(test_file_path)
+    def add_numbers(a, b):
+        return a + b
+
+
+    add_numbers("3", 5)
+
+    # Проверяем существование файла
+    assert os.path.exists(test_file_path)
+
+    # Проверяем содержимое
+    with open(test_file_path, 'r') as file:
+        content = file.read().strip()
+        assert content == "add_numbers error: TypeError. Inputs: '3', 5", f"Неверное содержимое файла: {content}"
+
+    os.remove(test_file_path)
